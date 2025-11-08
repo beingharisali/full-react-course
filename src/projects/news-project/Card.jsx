5c64e5838dc06e57b843d1d2d9e303ab527e2ba4
beingharisali <theharis99@gmail.com>
2025-11-08 11:40:34 +0500

    added tailwind css

--- src/projects/news-project/Card.jsx
+++ src/projects/news-project/Card.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Card({ title, description, author, url, urlToImage }) {
+function Card({ id, title, description, author, url, urlToImage }) {
+  const [readMore, setReadMore] = useState(false);
+  // const shortenDesc = description && description.substring(0, 50);
+  // console.log(shortenDesc);
   return (
-    <div className="card">
+    <div className="card w-80 rounded-md p-2 shadow-xl/20 my-4">
       <img src={urlToImage} alt="news" />
-      <h4>{title}</h4>
-      <p>{description}</p>
-      <div className="news-footer">
+      <h4 className="font-semibold my-2">{title}</h4>
+      <p>
+        {readMore
+          ? description && description
+          : description && description.substring(0, 50)}
+        <button
+          className="readmore-btn ml-2 cursor-pointer"
+          onClick={() => setReadMore(!readMore)}
+        >
+          {readMore ? "Show less" : "Read more"}
+        </button>
+      </p>
+      <div className="news-footer flex justify-between">
         <button>{author}</button>
         <a target="_blank" href={url}>
           Details
@@ -17,3 +30,11 @@ function Card({ title, description, author, url, urlToImage }) {
 }
 
 export default Card;
+
+// if (readMore === true) {
+//   if (description === true) {
+//     description;
+//   } else {
+//     description.substring(0, 100);
+//   }
+// }
--- src/projects/news-project/News.jsx
+++ src/projects/news-project/News.jsx
@@ -1,27 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
-import "./news.css";
+// import "./news.css";
 
 const url =
   "https://newsapi.org/v2/everything?q=bitcoin&apiKey=4fa932807d3247a1ac6fe5048016fafc";
 function News() {
   const [news, setNews] = useState([]);
+  const [query, setQuery] = useState("");
   async function fetchNews() {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data.articles);
     setNews(data.articles);
   }
+  function changeHandler(e) {
+    console.log(e.target.value);
+    setQuery(e.target.value);
+  }
   useEffect(() => {
     fetchNews();
   }, []);
   return (
     <div>
-      <h1>News</h1>
-      <div className="news">
-        {news.map((meriNews) => {
-          return <Card {...meriNews} />;
-        })}
+      <div className="news-header flex flex-col items-center">
+        <h1>News</h1>
+        <div className="field">
+          <input
+            type="search"
+            className="border rounded-sm p-1 w-96 mt-2"
+            value={query}
+            onChange={changeHandler}
+          />
+        </div>
+      </div>
+      <div className="news flex flex-wrap justify-between w-[90%] mx-auto gap-2">
+        {news
+          .filter((meriNews) => {
+            return meriNews.title.toLowerCase().includes(query);
+          })
+          .map((meriNews) => {
+            return <Card {...meriNews} key={meriNews.title} />;
+          })}
       </div>
     </div>
   );
